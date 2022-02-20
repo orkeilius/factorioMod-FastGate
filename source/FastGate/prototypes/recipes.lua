c2604e08f608ec72b57fbf4bf8ed2b0f81a26223
orkeilius <orkeilius@users.noreply.github.com>
2022-02-20 09:57:59 +0100

    fix bug (having recipe before unlocking the tech)

--- source/FastGate/prototypes/recipes.lua
+++ source/FastGate/prototypes/recipes.lua
@@ -1,5 +1,4 @@
 local fg = table.deepcopy(data.raw["recipe"]["gate"])
-fg.enabled = true
 fg.name = "fastGate"
 fg.ingredients = {{"gate",4},{"electronic-circuit",10},{"steel-plate",10}}
 fg.result = "fastGate"
