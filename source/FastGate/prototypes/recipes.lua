a52ac5a10722463e42fca56f4dec5b9552eb9561
orkeilius <orkeilius@users.noreply.github.com>
2024-11-03 12:23:35 +0100

    migrate recipe to 2.0

--- source/FastGate/prototypes/recipes.lua
+++ source/FastGate/prototypes/recipes.lua
@@ -1,6 +1,11 @@
 local fg = table.deepcopy(data.raw["recipe"]["gate"])
 fg.name = "fastGate"
-fg.ingredients = {{"gate",4},{"electronic-circuit",10},{"steel-plate",10}}
-fg.result = "fastGate"
+fg.icon = "__FastGate__/graphics/icon.png"
+fg.ingredients = {
+    {type="item", name="gate",amount=4},
+    {type="item", name="electronic-circuit",amount=10},
+    {type="item", name="steel-plate",amount=10}
+}
+fg.results = {{type="item",name="fastGate", amount=1}}
 
 data:extend{fg}
